9e913760150b798061e7f6297271e89bd5802f25
Tom Leonardsson <tom.leo@hotmail.com>
2015-09-02 17:38:19 +0200

    Removed merging artifacts

--- Project/core/src/com/dreamteam/villageTycoon/frameworkTest/TestScene.java
+++ Project/core/src/com/dreamteam/villageTycoon/frameworkTest/TestScene.java
@@ -1,21 +1,19 @@
 package com.dreamteam.villageTycoon.frameworkTest;
 
-<<<<<<< HEAD
 import com.dreamteam.villageTycoon.characters.Controller;
-=======
+
 import com.dreamteam.villageTycoon.AssetManager;
->>>>>>> origin/master
+
 import com.dreamteam.villageTycoon.framework.Scene;
 
 public class TestScene extends Scene {
 	public TestScene() {
 		super();
-<<<<<<< HEAD
-		addObject(new TestObject());
+
+		//addObject(new TestObject(null));
 		addObject(new Controller());
-=======
+
 		AssetManager.load();
 		addObject(new TestObject(AssetManager.getTexture("test")));
->>>>>>> origin/master
 	}
 }
