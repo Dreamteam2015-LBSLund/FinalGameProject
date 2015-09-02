0bf8584d27b7b96e522c776a8ae6566524849582
Tom Leonardsson <tom.leo@hotmail.com>
2015-09-02 09:37:50 +0200

    Animation gametinme

--- Project/core/src/com/dreamteam/villageTycoon/framework/Animation.java
+++ Project/core/src/com/dreamteam/villageTycoon/framework/Animation.java
@@ -5,30 +5,32 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Animation extends Sprite {
 	private int currentFrame;
-	private int animationCount;
-	private int maxAnimationCount;
 	private int maxFrame;
 	private int minFrame;
+	
+	private float animationTime;
+	private float maxAnimationTime;
+	
 	private boolean vertical;
 	
 	public Animation(Texture sprite){
 		super(sprite);
 	}
 	
-	public Animation(Sprite sprite, int maxAnimationCount, int maxFrame, int minFrame, boolean vertical){
+	public Animation(Sprite sprite, float maxAnimationTime, int maxFrame, int minFrame, boolean vertical){
 		super(sprite);
-		setAnimation(maxAnimationCount, maxFrame, minFrame, vertical);
+		setAnimation(maxAnimationTime, maxFrame, minFrame, vertical);
 	}
 	
-	public void setAnimation(int maxAnimationCount, int maxFrame, int minFrame, boolean vertical) {
-		this.maxAnimationCount = maxAnimationCount;
+	public void setAnimation(float maxAnimationTime, int maxFrame, int minFrame, boolean vertical) {
+		this.maxAnimationTime = maxAnimationTime;
 		this.maxFrame = maxFrame;
 		this.minFrame = minFrame;
 		this.vertical = vertical;
 	}
 	
-	public void animate() {
-		animationCount += 1;
+	public void animate(float deltaTime) {
+		animationTime += deltaTime;
 		
 		if(!vertical)
 			this.setRegion(minFrame*this.getRegionWidth() + currentFrame*this.getRegionWidth(), 
@@ -40,9 +42,9 @@ public class Animation extends Sprite {
 		if(currentFrame == maxFrame)
 			currentFrame = 0;
 		
-		if(animationCount >= maxAnimationCount) {
+		if(animationTime >= maxAnimationTime) {
 			currentFrame += 1;
-			animationCount = 0;
+			animationTime = 0;
 		}
 	}
 	
@@ -51,6 +53,6 @@ public class Animation extends Sprite {
 	}
 	
 	public void setAnimationCount(int animationCount) {
-		this.animationCount = animationCount;
+		this.animationTime = animationCount;
 	}
 }
