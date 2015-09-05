bfe52fda2344e33f39aba02662350c73aec30698
Tom Leonardsson <tom.leo@hotmail.com>
2015-09-05 23:13:15 +0200

    Escape to exi

--- Project/core/src/com/dreamteam/villageTycoon/Game.java
+++ Project/core/src/com/dreamteam/villageTycoon/Game.java
@@ -2,6 +2,7 @@ package com.dreamteam.villageTycoon;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -28,6 +29,7 @@ public class Game extends ApplicationAdapter {
 	
 	public void update() {
 		currentScene.update(Gdx.graphics.getDeltaTime());
+		if(Gdx.input.isKeyPressed(Keys.ESCAPE)) Gdx.app.exit();
 	}
 
 	@Override
