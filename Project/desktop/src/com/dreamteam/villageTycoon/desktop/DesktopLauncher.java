f384400b3714987d764f309e872e5f7fba075235
Johannes Larsson <mrja1lisepic@gmail.com>
2015-09-07 11:02:54 +0200

    fullscreen stuff

--- Project/desktop/src/com/dreamteam/villageTycoon/desktop/DesktopLauncher.java
+++ Project/desktop/src/com/dreamteam/villageTycoon/desktop/DesktopLauncher.java
@@ -5,10 +5,18 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.dreamteam.villageTycoon.Game;
 
 public class DesktopLauncher {
+	final static boolean FULLSCREEN = false;
+	
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 800;
-		config.height = 450;
+		if (FULLSCREEN) {
+			config.width = config.getDesktopDisplayMode().width;
+			config.height = config.getDesktopDisplayMode().height;
+			config.fullscreen = true;
+		} else {
+			config.width = 800;
+			config.height = 450;
+		}
 		config.foregroundFPS = 0;
 		new LwjglApplication(new Game(), config);
 	}
