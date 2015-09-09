4bc6597e061bc6b7526a219c8b32cb4e364e2ed0
Tom Leonardsson <tom.leo@hotmail.com>
2015-09-09 14:19:49 +0200

    explosion class

--- Project/core/src/com/dreamteam/villageTycoon/effects/Explosion.java
+++ Project/core/src/com/dreamteam/villageTycoon/effects/Explosion.java
@@ -1,5 +1,17 @@
 package com.dreamteam.villageTycoon.effects;
 
-public class Explosion {
+import com.badlogic.gdx.math.Vector2;
+import com.dreamteam.villageTycoon.framework.Animation;
+import com.dreamteam.villageTycoon.framework.GameObject;
 
+public class Explosion extends GameObject {
+	public enum Type { NAPALM, EXPLOSION };
+	
+	private float radius;
+	
+	
+	public Explosion(Vector2 position, Animation sprite) {
+		super(position, sprite);
+	}
+	
 }
