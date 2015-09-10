6470f8408f947fab475aa31831d6b2bbca3969ca
Tom Leonardsson <tom.leo@hotmail.com>
2015-09-10 20:59:09 +0200

    Expolsion constructer, explosion is removed when the animation is doen

--- Project/core/src/com/dreamteam/villageTycoon/effects/Explosion.java
+++ Project/core/src/com/dreamteam/villageTycoon/effects/Explosion.java
@@ -7,11 +7,23 @@ import com.dreamteam.villageTycoon.framework.GameObject;
 public class Explosion extends GameObject {
 	public enum Type { NAPALM, EXPLOSION };
 	
+	private Type type;
+	
 	private float radius;
+	private float fireDuration;
 	
-	
-	public Explosion(Vector2 position, Animation sprite) {
+	public Explosion(Vector2 position, Animation sprite, Type type, float radius, float fireDuration) {
 		super(position, sprite);
+		this.type = type;
+		this.radius = radius;
+		sprite.setSize(radius, radius);
 	}
 	
+	public void update(float deltaTime) {
+		super.update(deltaTime);
+		
+		if(getSprite().animationDone()) {
+			getScene().removeObject(this);
+		}
+	}
 }
