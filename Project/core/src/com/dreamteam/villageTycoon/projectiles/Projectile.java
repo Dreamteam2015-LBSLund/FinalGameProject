80dd0b443fdb7e798104b57c37801c6f4e0ed619
Tom Leonardsson <tom.leo@hotmail.com>
2015-09-23 15:26:35 +0200

    ProjectileType, started working on projectile

--- Project/core/src/com/dreamteam/villageTycoon/projectiles/Projectile.java
+++ Project/core/src/com/dreamteam/villageTycoon/projectiles/Projectile.java
@@ -1,5 +1,38 @@
 package com.dreamteam.villageTycoon.projectiles;
 
-public class Projectile {
+import com.badlogic.gdx.math.Vector2;
+import com.dreamteam.villageTycoon.framework.Animation;
+import com.dreamteam.villageTycoon.framework.GameObject;
 
+public class Projectile extends GameObject {
+	private ProjectileType projectileType;
+	
+	private float currentSpeed;
+	private float distanceTraveled;
+	private float alpha;
+	
+	private float angle;
+	
+	public Projectile(Vector2 position, ProjectileType projectileType) {
+		super(position, projectileType.getSprite());
+		
+		this.projectileType = projectileType;
+		alpha = 1;
+	}
+	
+	public void update(float deltaTime) {
+		super.update(deltaTime);
+		
+		distanceTraveled += currentSpeed * deltaTime;
+		
+		if(distanceTraveled >= projectileType.getMaxRange()) {
+			alpha -= deltaTime/10;
+		}
+		
+		this.getPosition().add(getVelocity(deltaTime));
+	}
+	
+	public Vector2 getVelocity(float deltaTime) {
+		return new Vector2((float)Math.cos(angle)*currentSpeed*deltaTime, (float)Math.sin(angle)*currentSpeed*deltaTime);
+	}
 }
--- Project/core/src/com/dreamteam/villageTycoon/projectiles/ProjectileType.java
+++ Project/core/src/com/dreamteam/villageTycoon/projectiles/ProjectileType.java
@@ -1,6 +1,7 @@
 package com.dreamteam.villageTycoon.projectiles;
 
 import com.dreamteam.villageTycoon.effects.Explosion;
+import com.dreamteam.villageTycoon.framework.Animation;
 
 public class ProjectileType {
 	public enum Type { SHOT, TRHOWABLE }; 
@@ -9,12 +10,24 @@ public class ProjectileType {
 	
 	private Explosion explosion;
 	
-	private int damege;
+	private Animation sprite;
 	
 	private float maxRange;
+	private float speed;
 	
-	public ProjectileType(Type type, int damege, float maxRange) {
-		
+	public ProjectileType(Type type, float maxRange, float speed, Animation sprite) {
+		this.type = type;
+		this.maxRange = maxRange;
+		this.speed = speed;
+		this.sprite = sprite;
+	}
+	
+	public float getSpeed() {
+		return speed;
+	}
+	
+	public Animation getSprite() {
+		return sprite;
 	}
 	
 	public Type getType() {
