df782ea1c4ff65454cf6bc5823cce89d0ea64814
Tom Leonardsson <tom.leo@hotmail.com>
2015-10-06 13:57:50 +0200

    removed unused imports

--- Project/core/src/com/dreamteam/villageTycoon/characters/Controller.java
+++ Project/core/src/com/dreamteam/villageTycoon/characters/Controller.java
@@ -6,8 +6,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.dreamteam.villageTycoon.AssetManager;
