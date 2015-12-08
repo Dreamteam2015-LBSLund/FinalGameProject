29558e564310e2167e01f95ed0bfee950ed4b665
Johannes Larsson <mrja1lisepic@gmail.com>
2015-12-08 13:56:10 +0100

    some city ai stuff

--- Project/core/src/com/dreamteam/villageTycoon/ai/AIController.java
+++ Project/core/src/com/dreamteam/villageTycoon/ai/AIController.java
@@ -8,10 +8,31 @@ import com.dreamteam.villageTycoon.buildings.BuildingType;
 import com.dreamteam.villageTycoon.buildings.City;
 
 public class AIController extends CityController {
+	
+	private static BuildingType[] buildOrder = new BuildingType[] {
+		BuildingType.getTypes().get("basicMine")
+	};
+	
+	private int buildOrderIndex;
+	Building building;
+	
 	City city;
 	
+	City playerCity; // in the future it should have other ai's as enemies too?
+	
+	public AIController() {
+		buildOrderIndex = 0;
+	}
+	
 	public void update() {
+		System.out.println("new city at " + city.getPosition());
+		if (playerCity != null) {
+			attackPlayer();
+		} else {
+			scout();
+		}
 		
+		build();
 	}
 	
 	private boolean build(BuildingType type, Vector2 position) {
@@ -23,5 +44,25 @@ public class AIController extends CityController {
 		} else {
 			return false;
 		}
+	}
+	
+	private void build() {
+		if (building == null || building.isBuilt()) {
+			if (buildOrderIndex < buildOrder.length) {
+				city.addBuilding(building = new Building(getNextBuildingPosition(), buildOrder[buildOrderIndex++], city), true);
+			}
+		}
+	}
+	
+	private Vector2 getNextBuildingPosition() {
+		return city.getPosition();
+	}
+	
+	private void scout() {
+		
+	}
+	
+	private void attackPlayer() {
+		
 	}
 }
