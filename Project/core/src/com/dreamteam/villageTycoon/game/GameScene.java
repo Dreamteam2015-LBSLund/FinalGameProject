004cb056ae73529f43e5e71d557ec2bd45147e14
Tom Leonardsson <tom.leo@hotmail.com>
2015-12-21 19:57:06 +0100

    added gameScene

---
+++ Project/core/src/com/dreamteam/villageTycoon/game/GameScene.java
@@ -0,0 +1,16 @@
+package com.dreamteam.villageTycoon.game;
+
+import com.dreamteam.villageTycoon.AssetManager;
+import com.dreamteam.villageTycoon.framework.Scene;
+
+public class GameScene extends Scene {
+	
+	public GameScene() {
+		super();
+		AssetManager.load();
+	}
+	
+	public void update(float dt) {
+		super.update(dt);
+	}
+}
