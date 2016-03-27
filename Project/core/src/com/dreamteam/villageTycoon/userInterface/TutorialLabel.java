6a5f592b2c09f3da0a0f1a658611383b7075ee9e
Tom Leonardsson <tom.leo@hotmail.com>
2016-03-27 22:53:48 +0200

    started working on tutorial

---
+++ Project/core/src/com/dreamteam/villageTycoon/userInterface/TutorialLabel.java
@@ -0,0 +1,28 @@
+package com.dreamteam.villageTycoon.userInterface;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class TutorialLabel {
+	final float NEXT_PART_TIME = 1;
+	
+	private Vector2 position;
+	
+	private String[] parts;
+	
+	private int currentPart;
+	
+	private float changePartCount;
+	
+	public TutorialLabel(Vector2 position) {
+		
+	}
+	
+	public void update(float deltaTime) {
+		
+	}
+	
+	public void draw(SpriteBatch batch) {
+		
+	}
+}
