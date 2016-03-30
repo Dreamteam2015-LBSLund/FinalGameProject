3e45780957499efef5879cef95c101278a1b1453
Tom Leonardsson <tom.leo@hotmail.com>
2016-03-30 09:13:31 +0200

    Tutorial text

--- Project/core/src/com/dreamteam/villageTycoon/userInterface/TutorialLabel.java
+++ Project/core/src/com/dreamteam/villageTycoon/userInterface/TutorialLabel.java
@@ -17,9 +17,14 @@ public class TutorialLabel {
 	
 	public TutorialLabel(Vector2 position) {
 		this.position = position;
+		
+		parts = new String[4];
+		
+		parts[0] = "Left click, or left click and drag to select characters \nRight click to send them to a position or building \nThey attack and work autonomously";
+		parts[1] = "Build factories to create weapons for soldiers \nRight click on selected character to bring up inventory \nPress on the sabotagekit to use it";
+		parts[2] = "Destroy all houses and kill all characters to defeat a village \nMore characters at a house or barrack ups the production speed of characters";
+		parts[3] = "Build buildings using the building placer \nZoom out with the arrows at Zoom \nChange game speed with the arrows at Speed";
 	}
-	
-	// TODO: write the tutuorial
 	
 	public void update(float deltaTime) {
 		if(currentPart < parts.length-1) {
